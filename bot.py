bd7e1985ea825263274105a292d5a74f309fcfe6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-23 19:08:15 +0300

    Registered test method to select a language

--- bot.py
+++ bot.py
@@ -1,8 +1,21 @@
+from aiogram import Bot, Dispatcher, executor, types 
+
+
 class TelegramBot:
     def __init__(self, api_token: str):
         if not isinstance(api_token, str) or api_token == "":
             print("ERROR: token must be a non empty string")
 
-    def run(self):
-        pass
+        self.bot = Bot(token=api_token)
+        self.dispatcher = Dispatcher(self.bot)
 
+        self.dispatcher.register_message_handler(
+                self.select_language, 
+                commands=["start"]
+            )
+
+    def run(self):
+        executor.start_polling(self.dispatcher)
+
+    async def select_language(self, message: types.Message):
+        await message.answer("Language selection")
