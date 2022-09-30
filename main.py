bbf5e3db22fa2e61f2cae140afff8aefcd0c2bcf
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-09-30 14:58:56 +0300

    Creates db object

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from dotenv import dotenv_values
 
+from db import DB
 from bot import TelegramBot
 
-config = dotenv_values(".env")
+config = dotenv_values("envs/.bot")
 
 if __name__ == "__main__":
+    database = DB()
+    database.create()
+
     bot = TelegramBot(api_token=config["TOKEN"])
     bot.run()
